eb4d76d295d0f072c56e0a28ccd167fbab55e063
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-08-03 23:23:11 +0600

    DS Final Assignment

--- Stack.cpp
+++ Stack.cpp
@@ -1,34 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define size 5
 using namespace std;
 
 int s[size];
 int top = 0;
 
-int menu(void)
+void push()
 {
-    int choice = -1;
-    while(choice < 0 || choice > 2)
+    if (top == size - 1)
     {
-        cout<<"1--push"<<endl;
-        cout<<"2--pop"<<endl;
-        cout<<"0--exit"<<endl;
-
-        cout<<"Enter Your choice : ";
-        cin>>choice;
-    }
-    return choice;
-}
-
-void push(){
-    if(top == size-1)
-    {
-        cout<<"Stack Overflow"<<endl;
+        cout << "Stack Overflow" << endl;
         return;
     }
     int value;
-    cout<<"Enter value: ";
-    cin>>value;
+    cout << "Enter value: ";
+    cin >> value;
 
     top++;
     s[top] = value;
@@ -36,51 +22,67 @@ void push(){
 
 void pop()
 {
-    if(top == 0)
+    if (top == 0)
     {
-        cout<<"Stack Underflow"<<endl;
+        cout << "Stack Underflow" << endl;
         return;
     }
-    cout<<"Stack popped : "<< s[top]<<endl;
+    cout << "Stack popped : " << s[top] << endl;
     top--;
 }
 
 void display()
 {
-    if(top ==0 )
+    if (top == 0)
     {
-        cout<<"Stack is empty !!"<<endl;
+        cout << "Stack is empty !!" << endl;
         return;
     }
-    cout<<"Stack elements : ";
-    for(int i = 1; i <=top; i++)
+    cout << "Stack elements : ";
+    for (int i = 1; i <= top; i++)
     {
-        cout<<s[i]<<" ";
+        cout << s[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
 
 int main()
 {
     int choice;
+    while (true)
+    {
+        cout << "\n--- Priority Queue Menu ---\n";
+        cout << "1. Insert\n";
+        cout << "2. Delete\n";
+        cout << "3. Display\n";
+        cout << "4. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
 
-    do {
-        choice = menu();
-
-        if(choice == 1)
+        switch (choice)
         {
+        case 1:
             push();
-            display();
-        }
-        else if(choice == 2)
-        {
+            break;
+
+        case 2:
             pop();
+            break;
+
+        case 3:
             display();
+            break;
+
+        case 4:
+            cout << "Exiting...\n";
+            return 0;
+
+        default:
+            cout << "Invalid choice\n";
         }
+    }
 
-    } while(choice != 0);
-
-    cout<<"End of operation"<<endl;
+    cout << "End of operation" << endl;
 
     return 0;
 }
